22a4ecd3a7759f5bd040b5793282b1eecd169213
github username <github email address>
2016-07-05 14:39:37 -0400

    completed

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,8 +14,9 @@
 /*: question1
 ### 1. Create a variable which represents your bank account balance. (For example: What is a good name for this variable that makes it easily readable for myself now and for the future me _or_ other developers I might be working with? Should I declare it using `let` or `var`? Should it be of type `Int` or `String`?)
 */
-// write your code here
+// write your code here -- i think it should be a float and it will decrease or hopefully increase in time
 
+var bankAccountBalance: Float = 100
 
 
 
@@ -24,6 +25,7 @@
 */
 // write your code here
 
+let nameOfMyPuppy = "Bella"
 
 
 
@@ -31,7 +33,7 @@
 ### 3. Use the `print()` function to print the name of your new puppy to the console.
 */
 // write your code here
-
+print("my new puppy's name is \(nameOfMyPuppy).")
 
 
 
@@ -39,7 +41,7 @@
 ### 4. Use the `print()` function to print the sentence "I just got a new puppy named <your puppy's name> and she is awesome!" to the console.
 */
 // write your code here
-
+print("I just got a new puppy named \(nameOfMyPuppy) and she is awesome!")
 
 
 
@@ -47,7 +49,7 @@
 ### 5. Use the `print()` function to print the sentence "I have $<balance> in my bank account." to the console.
 */
 // write your code here
-
+print("I have $\(bankAccountBalance) in my bank account.")
 
 
 
@@ -55,16 +57,17 @@
 ### 6. Congratulations! You just got $100 for your birthday, so now you have $100 more in your bank account. Update your bank account with the new balance and print "I now have $<balance>." to the console.
 */
 // write your code here
-
+bankAccountBalance += 100
+print("I now have $\(bankAccountBalance).")
 
 
 
 /*: question7
 ### 7. You decided you don't like the name Bella. Change your puppy's name to something else. (Can you do this? What happens when you try? Why?)
 */
-// write your code here
-
-
+// write your code here - it's a constant so you can't change it unless you assign it to a new var , or change the name to a var instead of a let/constant
+var myPuppysNewName = nameOfMyPuppy
+myPuppysNewName = "Nutella"
 
 
 //: [Solution](solution)
